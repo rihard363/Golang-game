379ccafeb627e9fa3fb9a47e7d963a71112b49a4
rihard363 <Strag_king2@mail.ru>
2017-03-24 10:12:20 +0300

    телефоная книга

--- learn/chapter4/phoneBook.go
+++ learn/chapter4/phoneBook.go
@@ -9,45 +9,53 @@ func main() {
     entry := map[string]string{
         name: number,
     }
-    var i int
-    fmt.Print("1. Показать книгу")
-    fmt.Print("2. Добавить контакт")
-    fmt.Print("3.Удалить контакт")
-    fmt.Print("4. Найти контакт")
-    fmt.Scanf("%i",&i)
-    switch i{
-      case 0: listEntry(&entry)
-      case 1: newEntry(&entry)
-      case 2: deleteEntry(&entry)
-      case 3: searchEntry(&entry)
+  Loop:
+    for {
+      var i int
+      fmt.Println("1. Показать книгу")
+      fmt.Println("2. Добавить контакт")
+      fmt.Println("3. Удалить контакт")
+      fmt.Println("4. Найти контакт")
+      fmt.Println("5. Выход")
+      fmt.Scanf("%d",&i)
+      switch i{
+      case 1: listEntry(&entry)
+      case 2: newEntry(&entry)
+      case 3: deleteEntry(&entry)
+      case 4: searchEntry(&entry)
+      case 5: break Loop
       default: fmt.Println("Данной команды не существует")
+        }
     }
-}
+  }
 func listEntry(entry *map[string]string) {
     for i, v:= range *entry{
         fmt.Println( i,v)
     }
 }
-func newEntry(entry *map[string]string) (name,number string){
+func newEntry(entry *map[string]string){
+  var name string
   fmt.Print("Введите имя: ")
   fmt.Scanf("%s", &name)
+  var number string
   fmt.Print("Введите телефон: ")
   fmt.Scanf("%s", &number)
-  *entry = map[string]string{
-      name: number,
-    }
+  (*entry)[name]= number
+  return 
 }
 func deleteEntry(entry *map[string]string){
-  var n int
+  var n string
   fmt.Print("Введите номер записи которую хотите удалить: ")
-  fmt.Scanf("%i", &n)
+  fmt.Scanf("%s", &n)
   delete(*entry,n)
+  return 
 }
 func searchEntry(entry *map[string]string){
  var search string
  fmt.Print("Введите имя которое нужно найти: ")
   fmt.Scanf("%s", &search)
- if name, ok := *entry["search"]; ok {    
-    fmt.Println(search, ok)
+ if name, ok := (*entry)[search]; ok {    
+    fmt.Println(name)
 } 
 }
+
